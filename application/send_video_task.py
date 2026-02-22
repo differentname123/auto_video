0761deefe84cc06f6e2ae0d6924e3ec052dcdb66
differentname123 <2434954452@qq.com>
2026-02-22 15:42:41 +0800

    2025-02-11:     已完成:         1.完成字幕的识别以及修复和还原     待完成：         -1:重构和重新实现字幕和水印遮挡这部分代码         -0.5：热门视频的挖掘感觉还是不是很ok，还需要优化         0.优化字幕形式(不使用模糊,而是直接特色化的背景框).优化画面和音频的长度,先丢弃多余的画面.然后想办法让文案变得更加和原来差不多一点.考虑实现对所有字幕的替换,和字幕的无损去除         1.感觉创作的文案，经常出现新文案和原始文案数量没对齐的情况，或者有时候拼接的效果还是不够好很牵强，故事不够完整，过渡语句很打断节奏，太频繁的问题。字幕特别不好看的问题，以及有时候字幕遮挡全黑太影响体验了。考虑到游戏视频挖掘的时候可以找没有声音的，或者是如何更好的挖掘优质的方案         2.存在创作的大问题，那就是有时候 (BV1zsrfBvEZD) 第4点 分成三个部分 等这些语言和时间内容不匹配，太照抄照搬了.BV1EyknBwE2b存在不完美的遮挡，以及bgm声音太小         3.有必要取消素材中的是否实时视频，直接在创作设置是否立即发送，有必要进行视频制作的尽量统一，一个是为了减小时间消耗，另一个是降低画质和音质的损失,排查视频制作过程中的瓶颈，进行优化         4.增加未发出视频的撤回功能，重试的按钮         5.b站的视频推荐活动也可以安排上了，争取激励金         6.进一步的提高有趣的字幕，甚至水印框的位置也应该增加我们自己合适的水印，考虑实现对别人字幕的去除之后再增加，比如直接截断然后在下面纯黑增加字幕         7.文案的优化:有时候差得太多字数内容了    BV1bjz8BhEzF新文案     "他看穿了马晴的纠结，不强求，不退缩，也不像某些人那样恼羞成怒，而是摆明车马：我懂你，我等你。",                                    老文案         "上一期他知道马晴对林健感兴趣，他没有像刘玫一样强行刷存在感，没有像大厨一样直接退缩，也没有像徐工一样看见女嘉宾选别人就上去质问，反而恰到好处的点出，我知道你的意图，但我尊重你的选择，而且我对你的态度依旧很积极。",             作者声音没有完全的对齐: 原文案:        "两个人一起吃晚饭时，他没有抱怨经费少，吃的不好，自己委屈，而是率先夸马晴。",             新的语音没有遮挡 "而是率先夸马晴" 这个语音         8.封面特别重要，允许用户上传封面.然后顺便新增是否自动发的功能，不自动发就会等待用户确定之后再发，而且前端要有明晃晃的元素提醒用户，免得用户忘记审批了。

--- application/send_video_task.py
+++ application/send_video_task.py
@@ -1114,7 +1114,7 @@ def send_good_plan(manager):
     :param manager:
     :return:
     """
-    need_process_users = ['hong', 'dahao', 'junda', 'mama', 'xue', 'danzhu', 'xiaoxiaosu', 'qiuru', 'nana', 'xiaomu', 'shun', 'ping', 'zhong', 'dan', 'qizhu', 'xiaocai']
+    need_process_users = ['hong', 'dahao', 'junda', 'mama', 'xue', 'danzhu', 'xiaoxiaosu', 'qiuru', 'nana', 'xiaomu', 'shun', 'ping', 'zhong', 'liuzhu', 'qizhu', 'xiaocai']
     user_detail_upload_info = gen_user_detail_upload_info(manager, need_process_users)
     all_video_info = query_all_material_videos(manager, False)
 
